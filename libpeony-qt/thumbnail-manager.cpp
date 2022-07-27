61b07d0e942f8a604b742aa83e3c0a100a6ee55b
Yue-Lan <lanyue@ubuntukylin.com>
2022-07-27 21:00:36 +0800

    尝试修复文件管理器退出时ThumbnailJob的崩溃问题

--- libpeony-qt/thumbnail-manager.cpp
+++ libpeony-qt/thumbnail-manager.cpp
@@ -44,6 +44,8 @@
 #include <QThreadPool>
 #include <QSemaphore>
 
+#include <QGuiApplication>
+
 #include <gio/gdesktopappinfo.h>
 
 using namespace Peony;
@@ -72,6 +74,11 @@ ThumbnailManager::ThumbnailManager(QObject *parent) : QObject(parent)
     m_semaphore = new QSemaphore(1);
 
     findAtril();
+
+    connect(qApp, &QGuiApplication::lastWindowClosed, this, [=]{
+        m_thumbnail_thread_pool->clear();
+        m_thumbnail_thread_pool->waitForDone(500);
+    });
 }
 
 ThumbnailManager::~ThumbnailManager()
