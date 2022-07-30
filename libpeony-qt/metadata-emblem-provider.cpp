1af8ca8ef08c3e008bdb1d5a728683167e39b859
Yue-Lan <lanyue@ubuntukylin.com>
2022-07-30 11:21:20 +0800

    FileMetaInfo: 添加dupFromUri接口，用于线程使用
    MetadataEmblemProvider: 使用dupFromUri方法获取FileMetaInfo，以避免线程安全问题

--- libpeony-qt/metadata-emblem-provider.cpp
+++ libpeony-qt/metadata-emblem-provider.cpp
@@ -19,13 +19,9 @@ const QString MetadataEmblemProvider::emblemKey()
 
 QStringList MetadataEmblemProvider::getFileEmblemIcons(const QString &uri)
 {
-    auto metaInfo = FileMetaInfo::fromUri(uri);
+    auto metaInfo = FileMetaInfo::dupFromUri(uri);
     if(!metaInfo || !metaInfo.get())
         return QStringList();
-    auto data = metaInfo->getMetaInfoStringListV1("emblems");
-    if (!data.isEmpty()) {
-        QStringList tmp = data;
-    }
     return metaInfo->getMetaInfoStringListV1("emblems");
 }
 
