c9432bc2b6251132150d753ac02e7b843bd8dcf9
HeMeihong <hemeihong@kylinos.cn>
2022-09-20 15:47:13 +0800

    Fix create folder in mobile deviece not sync issue, link to bug#132326.

--- libpeony-qt/file-operation/create-template-operation.cpp
+++ libpeony-qt/file-operation/create-template-operation.cpp
@@ -179,6 +179,12 @@ retry_create_template:
 
     GFile *dest_dir_file = g_file_new_for_uri(FileUtils::urlEncode(m_dest_dir_uri).toUtf8().constData());
     GMount *dest_dir_mount = g_file_find_enclosing_mount(dest_dir_file, nullptr, nullptr);
+
+    //fix create folder not sync issue, bug#128665, 132326
+    if (m_type == EmptyFolder) {
+        dest_dir_file = g_file_new_for_uri(FileUtils::urlEncode(m_target_uri).toUtf8().constData());
+    }
+
     if (src_first_mount) {
         needSync = g_mount_can_unmount(dest_dir_mount);
         g_object_unref(dest_dir_mount);
