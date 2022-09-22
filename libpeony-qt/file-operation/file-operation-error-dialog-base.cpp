6dd707e4a4fee62f4c63a07f6a113e442d1e01d9
yangling <yangling@kylinos.cn>
2022-09-22 13:38:07 +0800

    hotfix bug#139766 【文件管理器】【PTOF】清空回收站弹框界面中“取消”和“确定”按钮连接在一起

--- libpeony-qt/file-operation/file-operation-error-dialog-base.cpp
+++ libpeony-qt/file-operation/file-operation-error-dialog-base.cpp
@@ -110,7 +110,6 @@ Peony::FileOperationErrorDialogBase::FileOperationErrorDialogBase(QDialog *paren
 
     QHBoxLayout* buttonLayout = new QHBoxLayout;
     buttonLayout->setContentsMargins (0, 0, 13, 3);
-
     buttonLayout->addLayout (m_buttonLeft);
     buttonLayout->addStretch ();
     buttonLayout->addLayout (m_buttonRight);
@@ -189,6 +188,11 @@ QCheckBox *Peony::FileOperationErrorDialogBase::addCheckBoxLeft(QString name)
     }
 
     return nullptr;
+}
+
+void Peony::FileOperationErrorDialogBase::setButtonSpacing(int spacing)
+{
+    m_buttonRight->setSpacing(spacing);
 }
 
 bool Peony::FileOperationErrorDialogBase::event(QEvent *event)
