a436677528dcdd252189a0bece36bcb5ffb5aee3
李卓安 <lizhuoan@kylinos.cn>
2022-11-18 14:48:15 +0800

    fix bug 148806, matches end path name

--- libpeony-qt/file-operation/file-operation-error-dialogs.cpp
+++ libpeony-qt/file-operation/file-operation-error-dialogs.cpp
@@ -119,7 +119,8 @@ void Peony::FileOperationErrorDialogConflict::handle (FileOperationError& error)
         setTipFilename(file.getInfo()->displayName());
     } else {
         QString fileName = error.srcUri.split("/").back();
-        QString url = error.destDirUri.contains(fileName) ? error.destDirUri : error.destDirUri + "/" + fileName;
+        //fix bug 148806, matches end path name
+        QString url = error.destDirUri.split("/").back().contains(fileName) ? error.destDirUri : error.destDirUri + "/" + fileName;
         FileInfoJob file(url, nullptr);
         file.querySync();
         setTipFileicon(file.getInfo()->iconName());
