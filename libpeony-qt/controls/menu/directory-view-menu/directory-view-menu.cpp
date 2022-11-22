95cd011bfff95037404adba69554d781d6580d37
likehomedream <like1@kylinos.cn.com>
2022-11-22 09:42:53 +0800

    【UKUI3.1】文件管理器重构/文件颜色标记右键菜单重构[LINK]task#45830

--- libpeony-qt/controls/menu/directory-view-menu/directory-view-menu.cpp
+++ libpeony-qt/controls/menu/directory-view-menu/directory-view-menu.cpp
@@ -1223,6 +1223,10 @@ const QList<QAction *> DirectoryViewMenu::constructMenuPluginActions()
                         action->setParent(this);
                         action->setObjectName(plugin->name());
                         addAction(action);
+                        qDebug()<< id<<"-==================-";
+                        if(id == "Peony File Labels Menu Extension"){
+                            l<<addSeparator();
+                        }
                     }
                 }
             }
