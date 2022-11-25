4ac1aed8c7dc13e99e42e9131ea71607f97a84ca
Yue-Lan <lanyue@ubuntukylin.com>
2022-11-25 17:33:59 +0800

    修复qt5.15和kf5的编译问题

--- peony-qt-desktop/desktop-icon-view.cpp
+++ peony-qt-desktop/desktop-icon-view.cpp
@@ -1000,8 +1000,8 @@ void DesktopIconView::openFileByUri(QString uri)
             data.setHostname();
             data.addPid(pid);
             QRect rect = info.get()->property("iconGeometry").toRect();
-            if (rect.isValid())
-                data.setIconGeometry(rect);
+//            if (rect.isValid())
+//                data.setIconGeometry(rect);
             data.setLaunchedBy(getpid());
             KStartupInfo::sendStartup(startInfoId, data);
 #else
@@ -2502,9 +2502,9 @@ bool DesktopIconView::execSharedFileLink(const QString uri)
                     data.setHostname();
                     data.addPid(pid);
                     QRect rect = info.get()->property("iconGeometry").toRect();
-                    if (rect.isValid()) {
-                        data.setIconGeometry(rect);
-                    }
+//                    if (rect.isValid()) {
+//                        data.setIconGeometry(rect);
+//                    }
                     data.setLaunchedBy(getpid());
                     KStartupInfo::sendStartup(startInfoId, data);
 #else
