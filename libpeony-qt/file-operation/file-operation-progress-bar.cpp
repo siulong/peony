80b19c5b1e0d3d932de03edff1f214e7f1bdac19
yangyanwei <yangyanwei@kylinos.cn>
2023-01-04 10:58:32 +0800

    close-cd #I64KZQ【文件管理器】文件管理器右键属性窗口有两层标题栏,修改复制窗口出现两层标题

--- libpeony-qt/file-operation/file-operation-progress-bar.cpp
+++ libpeony-qt/file-operation/file-operation-progress-bar.cpp
@@ -116,6 +116,7 @@ ProgressBar *FileOperationProgressBar::addFileOperation()
 void FileOperationProgressBar::showProgress(ProgressBar &progress)
 {
     if (m_progress_size > 0) {
+        kdk::UkuiStyleHelper::self()->removeHeader(this);
         progress.show();
         show();
     }
@@ -400,6 +401,7 @@ void FileOperationProgressBar::showDelay(int msec)
 {
     QTimer::singleShot(msec, this, [=] () {
         if (m_list_widget->count() > 0 && !m_error) {
+            kdk::UkuiStyleHelper::self()->removeHeader(this);
             show();
         }
     });
