a5bc3928f6b78d75182a0b921a2e0e1cddec0506
yangling <yangling@kylinos.cn>
2023-05-16 15:53:51 +0800

    hotfix bug#170788 【文件管理器】文管侧边栏页签按键盘左右键多次切换后闪退

--- libpeony-qt/model/side-bar-file-system-item.cpp
+++ libpeony-qt/model/side-bar-file-system-item.cpp
@@ -582,8 +582,6 @@ void SideBarFileSystemItem::findChildren()
             SideBarFileSystemItem* item = new SideBarFileSystemItem(volume.name(), volume, this, m_model);
             m_children->append(item);
             m_model->endInsertRows();
-            m_model->indexUpdated(this->lastColumnIndex());
-            m_model->dataChanged(item->firstColumnIndex(), item->lastColumnIndex());
         }
 
         if (FileUtils::isFileExsit("file:///data/usershare")) {
