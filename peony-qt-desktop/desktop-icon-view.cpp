6189cee72f74a0737dd2008a836a45942e7afc78
Yue-Lan <lanyue@ubuntukylin.com>
2023-07-03 11:59:32 +0800

    close #I5M6A1 将文件拖放到桌面的图标显示不支持
    https://e.gitee.com/openkylin-enterprise/dashboard?issue=I5M6A1

--- peony-qt-desktop/desktop-icon-view.cpp
+++ peony-qt-desktop/desktop-icon-view.cpp
@@ -2044,6 +2044,7 @@ void DesktopIconView::dragMoveEvent(QDragMoveEvent *e)
         QHoverEvent he(QHoverEvent::HoverLeave, e->posF(), e->posF());
         viewportEvent(&he);
     }
+    e->setDropAction(action);
     if (e->isAccepted())
         return;
     qDebug()<<"drag move event" <<action;
@@ -2051,7 +2052,6 @@ void DesktopIconView::dragMoveEvent(QDragMoveEvent *e)
         e->accept();
         return QListView::dragMoveEvent(e);
     }
-    e->setDropAction(action);
     e->accept();
 }
 
