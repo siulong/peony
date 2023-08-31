e1107515147039c2fd4a40f511a01dabf25a34e9
Yue-Lan <lanyue@kylinos.cn>
2023-08-31 17:33:32 +0800

    增加XdgPortalHelper类，用于在peony和peony-qt-desktop初始化时屏蔽GTK_USE_PORTAL对trash流程的影响
    fixme:
    后续还是要对接xdg-desktop-portal，等xdp-ukui支持回收操作之后即可不做此处理，gappinfo也需要对接xdp的实现

--- libpeony-qt/convenient-utils/convenient-utils.pri
+++ libpeony-qt/convenient-utils/convenient-utils.pri
@@ -8,11 +8,13 @@ HEADERS += \
     $$PWD/clipboard-utils.h \
     $$PWD/datacdrom.h \
     $$PWD/file-operation-utils.h \
-    $$PWD/systemd-bus-accounts.h
+    $$PWD/systemd-bus-accounts.h \
+    $$PWD/xdg-portal-helper.h
 
 SOURCES += \
     $$PWD/audio-play-manager.cpp \
     $$PWD/clipboard-utils.cpp \
     $$PWD/datacdrom.cpp \
     $$PWD/file-operation-utils.cpp \
-    $$PWD/systemd-bus-accounts.cpp
+    $$PWD/systemd-bus-accounts.cpp \
+    $$PWD/xdg-portal-helper.cpp
--- src/peony-main.cpp
+++ src/peony-main.cpp
@@ -38,6 +38,8 @@
 
 #include "global-settings.h"
 
+#include "xdg-portal-helper.h"
+
 void messageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
     QByteArray localMsg = msg.toLocal8Bit();
@@ -84,6 +86,7 @@ void messageOutput(QtMsgType type, const QMessageLogContext &context, const QStr
 
 int main(int argc, char *argv[])
 {
+    Peony::XdgPortalHelper::getInstance()->tryUnusePortal();
     PeonyApplication::peony_start_time = QDateTime::currentMSecsSinceEpoch();
     initUkuiLog4qt("peony");
 //    qInstallMessageHandler(messageOutput);
@@ -107,6 +110,7 @@ int main(int argc, char *argv[])
     }
 
     PeonyApplication app(argc, argv, "peony-qt");
+    Peony::XdgPortalHelper::getInstance()->tryResetPortal();
     if (app.isSecondary())
         return 0;
 
