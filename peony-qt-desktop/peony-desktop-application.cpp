813bbe967f1c68442d28f9413edbc347fca772ea
Yue-Lan <lanyue@kylinos.cn>
2023-12-26 15:45:04 +0800

    [FIX] #205338 【文件管理器】在第一个用户上直接拔出已识别光盘的光驱，切换到第二个用户提示没有权限

--- peony-qt-desktop/peony-desktop-application.cpp
+++ peony-qt-desktop/peony-desktop-application.cpp
@@ -927,6 +927,11 @@ void guessContentTypeCallback(GObject* object, GAsyncResult *res,gpointer data)
     error = NULL;
     openFolder = true;
     root = g_mount_get_default_location(G_MOUNT(object));
+
+    // fix #205338
+    g_autoptr(GFileInfo) access_info = g_file_query_info(root, G_FILE_ATTRIBUTE_ACCESS_CAN_READ","G_FILE_ATTRIBUTE_ACCESS_CAN_EXECUTE, G_FILE_QUERY_INFO_NONE, nullptr, nullptr);
+    openFolder = g_file_info_get_attribute_boolean(access_info, G_FILE_ATTRIBUTE_ACCESS_CAN_READ) && g_file_info_get_attribute_boolean(access_info, G_FILE_ATTRIBUTE_ACCESS_CAN_EXECUTE);
+
     mountUri = g_file_get_uri(root);
 
     openFolderCmd = "/usr/bin/peony " + QString(mountUri);
