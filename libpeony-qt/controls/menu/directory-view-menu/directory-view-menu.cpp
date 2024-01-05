6d32334dc30f42e9b8304908540d3103c43d1595
Qxiangwenjie <xiangwenjie@kylinos.cn>
2024-01-05 16:51:11 +0800

    [FIX]【文件保护箱】通过文件管理器无法创建、卸载、删除、重命名文件保护箱[LINK]bug#208350

--- libpeony-qt/controls/menu/directory-view-menu/directory-view-menu.cpp
+++ libpeony-qt/controls/menu/directory-view-menu/directory-view-menu.cpp
@@ -1365,7 +1365,7 @@ const QList<QAction *> DirectoryViewMenu::constructMenuPluginActions()
             auto plugin = MenuPluginManager::getInstance()->getPlugin(id);
 
             if(m_is_filesafe||m_is_filebox_file) {
-                if(plugin->name() == tr("Peony-Qt filesafe menu Extension") || plugin->name() == tr("Peony File Labels Menu Extension")) {
+                if(plugin->name() == tr("Peony-Qt Filesafe Menu Extension") || plugin->name() == tr("Peony File Labels Menu Extension")) {
                     auto actions = plugin->menuActions(MenuPluginInterface::DirectoryView, m_directory, m_selections);
                     l<<actions;
                     for (auto action : actions) {
@@ -1375,7 +1375,7 @@ const QList<QAction *> DirectoryViewMenu::constructMenuPluginActions()
                     }
                 }
             } else {
-                if(plugin->name() != tr("Peony-Qt filesafe menu Extension")) {
+                if(plugin->name() != tr("Peony-Qt Filesafe Menu Extension")) {
                     auto actions = plugin->menuActions(MenuPluginInterface::DirectoryView, m_directory, m_selections);
                     l<<actions;
                     for (auto action : actions) {
