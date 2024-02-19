238651ed36f8855e47335b7b856b025026084797
Yue-Lan <lanyue@kylinos.cn>
2024-02-19 16:17:46 +0800

    修复属性窗口SDK编译选项未检查问题

--- libpeony-qt/windows/properties-window.cpp
+++ libpeony-qt/windows/properties-window.cpp
@@ -269,12 +269,15 @@ void PropertiesWindow::init()
     this->setAttribute(Qt::WA_DeleteOnClose);
     this->setContentsMargins(0, 0, 0, 0);
     this->setAttribute(Qt::WA_TranslucentBackground);
+#ifdef KY_SDK_WAYLANDHELPER
     kdk::UkuiStyleHelper::self()->removeHeader(this);
-//    MotifWmHints hints;
-//    hints.flags = MWM_HINTS_FUNCTIONS|MWM_HINTS_DECORATIONS;
-//    hints.functions = MWM_FUNC_ALL;
-//    hints.decorations = MWM_DECOR_BORDER;
-//    XAtomHelper::getInstance()->setWindowMotifHint(window()->winId(), hints);
+#else
+    MotifWmHints hints;
+    hints.flags = MWM_HINTS_FUNCTIONS|MWM_HINTS_DECORATIONS;
+    hints.functions = MWM_FUNC_ALL;
+    hints.decorations = MWM_DECOR_BORDER;
+    XAtomHelper::getInstance()->setWindowMotifHint(window()->winId(), hints);
+#endif
     //only show close button
     //this->setWindowFlags(this->windowFlags() & ~Qt::WindowMinMaxButtonsHint & ~Qt::WindowSystemMenuHint);
 
@@ -696,6 +699,8 @@ void PropertiesWindow::paintEvent(QPaintEvent *event)
     QWidget::paintEvent(event);
 }
 
+
+#ifdef KY_SDK_QT_WIDGETS
 class TabBar : public kdk::KTabBar
 {
 public:
@@ -714,13 +719,15 @@ protected:
         return QTabBar::tabSizeHint(index);
     }
 };
+#endif
 
 //properties window
 PropertiesWindowPrivate::PropertiesWindowPrivate(const QStringList &uris, QWidget *parent) : QTabWidget(parent)
 {
+#ifdef KY_SDK_QT_WIDGETS
     auto tabbar = new TabBar(nullptr);
     setTabBar(tabbar);
-
+#endif
     setTabsClosable(false);
     setMovable(false);
     setContentsMargins(0, 0, 0, 0);
