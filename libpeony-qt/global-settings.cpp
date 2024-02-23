36e7825539b0000b0c698add33a8e54ccd9153ab
Yue-Lan <lanyue@kylinos.cn>
2024-02-23 10:43:50 +0800

    GlobalSettings: 添加主题widgetThemeName配置用于适配主题相关配置

--- libpeony-qt/global-settings.cpp
+++ libpeony-qt/global-settings.cpp
@@ -308,7 +308,9 @@ GlobalSettings::~GlobalSettings()
 
 void GlobalSettings::getUkuiStyle()
 {
+    bool transparentConfigFromUKUIStyle = true;
     if (QGSettings::isSchemaInstalled(UKUI_CONTROL_CENTER_PERSONALISE)) {
+        transparentConfigFromUKUIStyle = false;
         m_gsettings = new QGSettings(UKUI_CONTROL_CENTER_PERSONALISE);
         connect(m_gsettings, &QGSettings::changed, [this](const QString &key) {
             if (key == PERSONALISE_EFFECT) {
@@ -328,18 +330,28 @@ void GlobalSettings::getUkuiStyle()
         }
         m_cache.remove(SIDEBAR_BG_OPACITY);
         m_cache.insert(SIDEBAR_BG_OPACITY, opacity);
-    } else {
-        if (QGSettings::isSchemaInstalled("org.ukui.style")) {
-            m_gsettings = new QGSettings("org.ukui.style", QByteArray(), this);
-            connect(m_gsettings, &QGSettings::changed, this, [=](const QString &key) {
-                if (key == "peonySideBarTransparency") {
-                    m_cache.remove(SIDEBAR_BG_OPACITY);
-                    m_cache.insert(SIDEBAR_BG_OPACITY, m_gsettings->get(key).toString());
-                    qApp->paletteChanged(qApp->palette());
-                }
-            });
+    }
+    if (QGSettings::isSchemaInstalled("org.ukui.style")) {
+        m_peony_gsettings = new QGSettings("org.ukui.style", QByteArray(), this);
+        connect(m_peony_gsettings, &QGSettings::changed, this, [=](const QString &key) {
+            if (key == "peonySideBarTransparency" && transparentConfigFromUKUIStyle) {
+                m_cache.remove(SIDEBAR_BG_OPACITY);
+                m_cache.insert(SIDEBAR_BG_OPACITY, m_peony_gsettings->get(key).toString());
+                qApp->paletteChanged(qApp->palette());
+            }
+            if (key == "widgetThemeName") {
+                m_cache.remove("widgetThemeName");
+                m_cache.insert("widgetThemeName", m_peony_gsettings->get("widgetThemeName").toString());
+                Q_EMIT this->valueChanged("widgetThemeName");
+            }
+        });
+        if (transparentConfigFromUKUIStyle) {
             m_cache.remove(SIDEBAR_BG_OPACITY);
-            m_cache.insert(SIDEBAR_BG_OPACITY, m_gsettings->get("peonySideBarTransparency").toString());
+            m_cache.insert(SIDEBAR_BG_OPACITY, m_peony_gsettings->get("peonySideBarTransparency").toString());
+        }
+        if (m_peony_gsettings->keys().contains("widgetThemeName")) {
+            m_cache.remove("widgetThemeName");
+            m_cache.insert("widgetThemeName", m_peony_gsettings->get("widgetThemeName").toString());
         }
     }
 }
