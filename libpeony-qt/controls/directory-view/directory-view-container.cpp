e84d5f30b15d2d9ab3d507994cb02ee30b719526
renyangguang <renyangguang@kylinos.cn>
2024-10-10 14:11:15 +0800

    bugfix:[268879] 看图APP打开图片时执行sm-safeguard --remove -f bgset命令后看图app崩溃

--- libpeony-qt/controls/directory-view/directory-view-container.cpp
+++ libpeony-qt/controls/directory-view/directory-view-container.cpp
@@ -91,7 +91,7 @@ DirectoryViewContainer::DirectoryViewContainer(QWidget *parent) : QWidget(parent
         m_control_center_plugin = new QGSettings("org.ukui.control-center.panel.plugins", QByteArray(), this);
         connect(m_control_center_plugin, &QGSettings::changed, this, [=](const QString &key) {
            qDebug() << "panel settings changed:" <<key;
-           if (getView()->viewId() == "List View" && (key == "date" || key == "hoursystem"))
+           if (getView() && getView()->viewId() == "List View" && (key == "date" || key == "hoursystem"))
               refresh();
         });
     }
