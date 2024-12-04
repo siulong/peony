ed7a1fa8585289f5fdd31df968fb943f3ca18dc6
yanglg1 <yangling@kylinos.cn>
2024-12-04 11:48:55 +0800

    修改信号参数改变导致文件对话框打不开

--- src/control/search-widget.cpp
+++ src/control/search-widget.cpp
@@ -44,8 +44,9 @@ SearchWidget::SearchWidget(QWidget *parent) : QWidget(parent)
 
     connect(m_locationBar, &AdvancedLocationBar::updateFileTypeFilter, this, &SearchWidget::updateFileTypeFilter);
 
-    connect(m_locationBar, &AdvancedLocationBar::searchRequest, [=](const QString &path, const QString &key, const bool searchMode){
+    connect(m_locationBar, &AdvancedLocationBar::searchRequest, [=](const QString &path, const QString &key){
         //key is null, clean search content, show all files
+        bool searchMode = m_locationBar->getSearchMode();
         if (key == "" || key.isNull()) {
             Q_EMIT this->updateSearchRequest(searchMode);
             Q_EMIT this->updateLocationRequest(path, false);
