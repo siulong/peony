0c67aaea96f1cd644c8b5c018dfbc9376e3ca5c6
renyangguang <renyangguang@kylinos.cn>
2024-12-09 15:42:18 +0800

    bugfix:[#I9VNFB]【文件管理器】【次要】桌面上文件/文件夹的快捷方式删除到回收站后，图标显示异常

--- libpeony-qt/controls/directory-view/delegate/list-view-delegate.cpp
+++ libpeony-qt/controls/directory-view/delegate/list-view-delegate.cpp
@@ -242,7 +242,16 @@ void ListViewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opti
         //paint access emblems
         //NOTE: we can not query the file attribute in smb:///(samba) and network:///.
         if (info->uri().startsWith("file:")) {
-            if (!info->canRead()) {
+            /**
+             * @bug #262561: [File Manager] PDF desktop shortcut files with deleted source files
+             *  do not display the same icon on the desktop folder as on the desktop.
+             *
+             * If the source file of a symbolic link is deleted, an “X” icon will be displayed in the upper left corner.
+             *
+             * @author: Renyg <renyangguang@kylinos.cn>
+             * @date:   2024-09-11
+             */
+            if (!info->canRead()  || !info->isExistTargetOfSymlink()) {
                 emblemPoses.removeOne(1);
                 QIcon icon = QIcon::fromTheme("emblem-unreadable");
                 painter->save();
--- peony-qt-desktop/desktop-icon-view-delegate.cpp
+++ peony-qt-desktop/desktop-icon-view-delegate.cpp
@@ -284,7 +284,16 @@ void DesktopIconViewDelegate::paint(QPainter *painter, const QStyleOptionViewIte
     FileInfo* file = FileInfo::fromUri(uri).get();
     if ((uri != "computer:///") && (uri != "trash:///")) {
         auto linkRect = QRect(QPoint(topLeftX, topLeftY), QSize(emblemsSize, emblemsSize));
-        if (! file->canRead())
+        /**
+         * @bug #262561: [File Manager] PDF desktop shortcut files with deleted source files
+         *  do not display the same icon on the desktop folder as on the desktop.
+         *
+         * If the source file of a symbolic link is deleted, an “X” icon will be displayed in the upper left corner.
+         *
+         * @author: Renyg <renyangguang@kylinos.cn>
+         * @date:   2024-09-11
+         */
+        if (! file->canRead() || !file->isExistTargetOfSymlink())
         {
             emblemPoses.removeOne(1);
             QIcon symbolicLinkIcon = QIcon::fromTheme("emblem-unreadable");
